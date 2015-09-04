8c3681a218a1fcaa84ad48d7a2d6739497b3d0d3
Alexey Krasikov <krasikov@request.ru>
2015-09-04 20:17:54 +0300

    modify rfid mfrc522

---
+++ Rfid/DEBUG.h
@@ -0,0 +1,26 @@
+#ifndef DEBUG_H__
+#define DEBUG_H__
+
+#ifdef DEBUG
+#define ass(x)						x
+#define assert(x)                   Serial.print(x)
+#define assertln(x)                 Serial.println(x); delay(5)
+#define assert2(x, y)               Serial.print(x, y)
+#define assert2ln(x, y)             Serial.println(x, y); delay(5)
+#define assertF(x)                   Serial.print(F(x))
+#define assertlnF(x)                 Serial.println(F(x)); delay(5)
+#define assert2F(x, y)               Serial.print(F(x), y)
+#define assert2lnF(x, y)             Serial.println(F(x), y); delay(5)
+#define assertInit(x)               Serial.begin(x)
+#define assertFlush 				Serial.flush();
+#else
+#define ass(x)
+#define assert(x) 
+#define assertln(x)  
+#define assert2(x, y)
+#define assert2ln(x, y)                
+#define assertInit(x)
+#define assertFlush               
+#endif
+
+#endif
